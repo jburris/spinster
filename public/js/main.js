6a84c9f5fd315715415d9a981956cdee0c0475c0
Justin Burris <justinbburris@gmail.com>
2013-09-24 16:39:44 -0400

    Detection for when a user attempts to pin a photo

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
+$(function() {
+  $('body').on('click', 'i', function() {
+    var pinId = $(this).data('pin-id'),
+    repin = 'PIN_' + pinId + '_repin';
 
+    if($.inArray(repin, $(this).attr('class').split(','))) {
+      console.log('you tried to pin a photo!');
+    }
+  });
+});
